83d610c038a6884b79dfeb237eb539a62b13d31b
VivekPa <vivekpalaniappan69@gmail.com>
2018-08-13 17:10:21 +0800

    added python program

---
+++ binomial_option_model.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def binomial_model(N, S0, u, r, K):
+    d = 1/u
+    p = (1+r-d)/(u-d)
+    q = 1-p
+
+    # make stock price tree
+    stock = np.zeros([N+1, N+1])
+    for i in range(N+1):
+        for j in range(i+1):
+            stock[j, i] = S0*(u**(i-j))*(d**j)
+
+    # Generate option prices recursively
+    option = np.zeros([N+1, N+1])
+    option[:, N] = np.maximum(np.zeros(N+1), (stock[:, N]-K))
+    for i in np.arange(N-1, -1, -1):
+        for j in range(0, i+1):
+            option[j, i] = 1/(1+r) * (p*option[j, i+1]+q*option[j+1, i+1])
+
+    return option[0, 0]
+
+
+op_price = binomial_model(5, 4, 2, 0.25, 8)
+
+print(op_price)
