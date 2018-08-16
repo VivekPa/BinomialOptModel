7580db6b773906e1acaf56b9b638cb9716380474
Vivek Palaniappan <vivekpalaniappan69@gmail.com>
2018-08-16 16:18:32 +0800

    Update stock_option.py

--- stock_option.py
+++ stock_option.py
@@ -3,39 +3,39 @@ import math
 class stockoption():
 
 	def __init__(self, S0, K, r, T, N, prm):
-'''
-S0 = initial stock price
-K = strike price
-r = risk free interest rate per annum
-T = length of option in years
-N = number of binomial iterations
-prm = dictionary with additional parameters
-'''
-	self.S0 = S0
-	self.K = K
-	self.r = r
-	self.T = T
-	self.N = N
-'''
-further parameters:
+		'''
+		S0 = initial stock price
+		K = strike price
+		r = risk free interest rate per annum
+		T = length of option in years
+		N = number of binomial iterations
+		prm = dictionary with additional parameters
+		'''
+		self.S0 = S0
+		self.K = K
+		self.r = r
+		self.T = T
+		self.N = N
+		'''
+		further parameters:
 
-div = dividend paid
-sigma = volatility of stock
-is_call = call or put option
-eu_option = European or American option
-'''
-	self.div = prm.get('div', 0)
-	self.sigma = prm.get('sigma', 0)
-	self.is_call = prm.get('is_call', True)
-	self.eu_option = prm.get('eu_option', True)
+		div = dividend paid
+		sigma = volatility of stock
+		is_call = call or put option
+		eu_option = European or American option
+		'''
+		self.div = prm.get('div', 0)
+		self.sigma = prm.get('sigma', 0)
+		self.is_call = prm.get('is_call', True)
+		self.eu_option = prm.get('eu_option', True)
 
-'''
-derived values:
+		'''
+		derived values:
 
-dt = time per step, in years
-df = discount factor
-'''
-	self.dt = T/float(N)
-	self.df = math.exp(-(r-self.div)*self.dt)
+		dt = time per step, in years
+		df = discount factor
+		'''
+		self.dt = T/float(N)
+		self.df = math.exp(-(r-self.div)*self.dt)
 
 	
