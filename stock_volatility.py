22ac4074c03ac76dd03a616f3a8bc8aed0fee914
robertmartin8 <martin.robertandrew@gmail.com>
2018-08-21 15:04:15 +0100

    renamed class, fix formatting

--- stock_volatility.py
+++ stock_volatility.py
@@ -2,26 +2,26 @@ import numpy as np
 import pandas as pd
 import pandas_datareader.data as pdr
 import fix_yahoo_finance as yf
+
 yf.pdr_override()
 
 
-class stock_vol:
+class StockVol:
 
-	def __init__(self, tk, start, end):
-		self.tk = tk
-		self.start = start
-		self.end = end
-		all_data = pdr.get_data_yahoo(self.tk, start=self.start, end=self.end)
-		self.stock_data = pd.DataFrame(all_data['Adj Close'])
-		self.log_return = np.log(self.stock_data)-np.log(self.stock_data.shift(1))
+    def __init__(self, tk, start, end):
+        self.tk = tk
+        self.start = start
+        self.end = end
+        all_data = pdr.get_data_yahoo(self.tk, start=self.start, end=self.end)
+        self.stock_data = pd.DataFrame(all_data["Adj Close"])
+        self.log_return = np.log(self.stock_data) - np.log(self.stock_data.shift(1))
 
-	def mean_sigma(self):
-		st = self.log_return.ewm(span=252).std()
-		sigma = st.iloc[-1]
-		return sigma
+    def mean_sigma(self):
+        st = self.log_return.ewm(span=252).std()
+        sigma = st.iloc[-1]
+        return sigma
 
 
-'''
-s = stock_vol('AAPL', '2015-01-01', '2016-01-01')
-print(s.mean_sigma())
-'''
+if __name__ == "__main__":
+    s = StockVol("AAPL", "2015-01-01", "2016-01-01")
+    print(s.mean_sigma())
